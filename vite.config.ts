02ba8acc062bcfea1233e47bc9444ae44498f056
Dazbii <jlehmann96@gmail.com>
2024-09-21 03:44:17 -0400

    Trying a different base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/TimelineApp/"
+  base: "https://dazbii.github.io/TimelineApp/"
 })
